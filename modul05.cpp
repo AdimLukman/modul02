cda4774335a5a5e7f260cadc2bf6b4c885736d46
AdimLukman <32480217+AdimLukman@users.noreply.github.com>
2017-10-25 19:14:34 +0800

    finishing

--- modul05.cpp
+++ modul05.cpp
@@ -3,4 +3,19 @@ using namespace std;
 
 int main(){
 	
+	double Luas_rumah= 42;
+	double Lantai_parket= 193*1215;
+	double Luas_1box= Lantai_parket*8/1000000;
+	double Jml_box_parket,Biaya_parket,Biaya_pasang,Totalbyr;
+	
+	cout <<"Luas Rumah : "<<Luas_rumah<<"m2"<<endl;
+	cout <<"Luas 1 box parket : "<<Luas_1box<<"m2"<<endl;
+	Jml_box_parket=Luas_rumah/Luas_1box;
+	cout <<" jumlah Box Parket : "<<Jml_box_parket<<endl;
+	Biaya_parket=Jml_box_parket*500;
+	cout <<"Biaya Parket (Rp) : "<<Biaya_parket<<endl;
+	Biaya_pasang=Luas_rumah*20;
+	cout<<"Biaya pasang(Rp) :"<<Biaya_pasang<<endl;
+	Totalbyr=Biaya_parket+Biaya_pasang;
+	cout<<"Total Biaya(Rp) : "<<Totalbyr<<endl; 
 } 
